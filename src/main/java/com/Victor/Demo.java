0e13c54a4147fb91f0948e08bfd2874c04de390e
missi <missi@DESKTOP-01C3Q8S>
2022-05-20 16:36:42 -0500

    Master

---
+++ src/main/java/com/Victor/Demo.java
@@ -0,0 +1,12 @@
+package com.Victor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class Demo 
+{
+	@GetMapping("/")
+	public String Portada()
+	{
+		return "Portada";
+		}
+}
